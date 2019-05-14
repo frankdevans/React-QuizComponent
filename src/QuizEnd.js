08bd012f4cdac3b85c7b0c0d6e332956c12224e3
Frank D. Evans <frank.evans@sonicdrivein.com>
2019-05-13 22:27:54 -0400

    Completed the module

---
+++ src/QuizEnd.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+
+class QuizEnd extends Component {
+    render() {
+        const layout = (
+            <div>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+        return layout
+    }
+}
+
+export default QuizEnd
